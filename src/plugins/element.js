eab4574106ce1c29af72a5da67edc21c7189a92a
soband <982160130@qq.com>
2024-12-24 10:22:21 +0800

    解决项目启动报错的问题

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,7 @@
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import locale from "element-plus/lib/locale/lang/zh-cn";
+// import locale from "element-plus/lib/locale/lang/zh-cn";
+import locale from 'element-plus/es/locale/lang/zh-cn';
 // 注册全部的svg图标
 import elementIcons from "@/plugins/svgicon";
 import "virtual:svg-icons-register";
